6d4331ce34da9a7cd0f476c975072c4a49e68ae5
NewGen2022 <amirgimari@gmail.com>
2025-04-05 12:44:34 +0300

    upwork job website routes

---
+++ api/upWorkRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const router = express.Router();
+import { upworkScraperController } from './controllers.js';
+
+router.get('/jobs', async (req, res) => {
+    try {
+        const { q: keywords, page } = req.query;
+
+        const allJobs = await upworkScraperController(keywords, page);
+        res.status(200).json(allJobs);
+    } catch (err) {
+        console.log('Error getting jobs:', err);
+        res.status(500).json({ msg: 'Error getting jobs' });
+    }
+});
+
+export default router;
