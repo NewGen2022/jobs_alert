afcf27f031d24a5aa29d4c5904d14d3ac3907ce8
NewGen2022 <amirgimari@gmail.com>
2025-04-05 12:42:50 +0300

    server configuration

---
+++ api/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import upworkJobsRouter from './routes.js';
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = process.env.PORT || 3003;
+
+server.use('/api/upwork', upworkJobsRouter);
+
+server.listen(PORT, () => {
+    console.log('Server is running on port:', PORT);
+});
